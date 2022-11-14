2c971bd0be8915bafff7d4b2cdbc7059f8f8ab4c
123priyank <priyank9724@gmail.com>
2022-11-14 11:33:26 +0530

    second

---
+++ basic.js
@@ -0,0 +1,31 @@
+// javascript is client side language 
+
+// JavaScript Tutorial
+
+// JavaScript Introduction
+//JavaScript Example
+//External JavaScript
+
+// JavaScript Basics
+
+// JS Comment-----------complate
+// JS Variable-----------complate  
+// JS Global Variable-------complate
+// JS Data Types------------complate
+// JS Operators--------------complate
+// js google console
+// JS If Statement
+// JS Switch
+// JS Loop
+// JS Function
+
+
+// JavaScript Objects
+
+// JS Object
+// JS Array
+// JS String
+// JS Date
+// JS Math
+// JS Number
+// JS Boolean
---
+++ javascript-basic/js/external.js
@@ -0,0 +1,1 @@
+document.write("hey this is external script implimation in html file")
